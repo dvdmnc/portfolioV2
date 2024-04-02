05dd1239a63ce93d7172731c996d19fb4ab01600
dvdmnc <130088298+dvdmnc@users.noreply.github.com>
2024-04-02 20:54:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import HomeEnglish from './pages/HomeEnglish';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/english' element={<HomeEnglish />} />
+      <Route path='/contact' element={<Contact />} />
     </Routes>
   );
 }
